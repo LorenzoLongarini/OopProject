8582ecc8630e5e9c2e2814b91b49be4e66167199
Lorenzo Longarini <s1089145@studenti.univpm.it>
2020-09-07 22:21:29 +0200

    modifiche e prime prove su postman

--- objProject/src/main/java/it/univpm/objProject/services/RevisionServiceImpl.java
+++ objProject/src/main/java/it/univpm/objProject/services/RevisionServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 import it.univpm.objProject.model.Metadata;
 
+
 import org.springframework.stereotype.Service;
 
 import it.univpm.objProject.util.MetadataArray;
@@ -12,7 +13,8 @@ import it.univpm.objProject.util.MetadataArray;
 @Service
 public class RevisionServiceImpl implements RevisionService {
 
-	private MetadataArray ma;
+	
+	private MetadataArray ma = new MetadataArray();
 	
 	@Override
 	public ArrayList<Metadata> MetadataVisual(){
