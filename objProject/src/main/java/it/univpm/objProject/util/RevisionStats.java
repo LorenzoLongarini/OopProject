1da06a16d2c565fdc1536f41907c6a23a4118151
Lorenzo Longarini <s1089145@studenti.univpm.it>
2020-09-13 20:23:02 +0200

    eliminazione classi superflue e aggiustamenti nelle stats (restituzione delle 24 ore precedenti alla data inserita)

--- objProject/src/main/java/it/univpm/objProject/util/RevisionStats.java
+++ objProject/src/main/java/it/univpm/objProject/util/RevisionStats.java
@@ -14,27 +14,24 @@ import it.univpm.objProject.services.Database;
 public class RevisionStats {
 	
 	public Stats create_stats(JSONObject jobj){
-
-		FilteredRev frv = new FilteredRev();
-		frv.setServer_modified((String)jobj.get("key"));
 		
-		//ArrayList<Stats> st_arr = new ArrayList<Stats>();
+		Entry en1 = new Entry();
+		en1.setServer_modified((String)jobj.get("key"));
+		
 		Stats st = new Stats();
 		Database re2 = new Database();
 		ArrayList<Revision> re = re2.CreateDatabase(); 
 		
-		//String convert = jobj.toString();
 		
-		Instant convert_date = Instant.parse(frv.server_modified);
-		long convert_seconds = convert_date.getEpochSecond();
+		long convert_seconds1 = en1.server_modified.getEpochSecond();
+		long convert_seconds2 = convert_seconds1 - 86400;
 		
 		for(int i = 0; i < re.size(); i++) {
 			ArrayList<Entry> ent = re.get(i).getEntries();
 			for(int j = 0; j < ent.size(); j++) {
 				Entry en = ent.get(j);
 				long modify_sec = en.getServer_modified().getEpochSecond();
-				if(modify_sec <= convert_seconds) {
-					//st = new Stats();
+				if(modify_sec >= convert_seconds2 && modify_sec <= convert_seconds1) {
 					st.setRev_per_day(1);
 					
 				}
